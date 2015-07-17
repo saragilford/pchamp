876a17bfa5e485b9bc92bf457053534d7d16b2b5
Sara Gilford <sara.gilford@gmail.com>
2015-07-17 13:21:57 -0700

    Add formatting methods

--- lib/pchamp.rb
+++ lib/pchamp.rb
@@ -1,5 +1,36 @@
 require "pchamp/version"
 
 module Pchamp
-  # Your code goes here...
+  class Champ
+
+    def pc(response)
+      if response.is_a? String
+        format_string(response)
+      elsif response.is_a? Hash || response.is_a? Array || response.is_a? TrueClass || response.is_a? FalseClass || response.is_a? NilClass || response.is_a? Fixnum || response.is_a? Float
+        format_data(response)
+      elsif
+
+
+      end
+    end
+
+    def format_string(string)
+      puts ('*' * 40).blue
+      puts "\"#{string}\"".green
+      puts ('*' * 40).blue
+    end
+
+    def format_data(data)
+      puts ('*' * 40).blue
+      puts "#{data}".green
+      puts ('*' * 40).blue
+    end
+
+    def
+
+
+
+
+
+  end
 end
--- spec/pchamp_spec.rb
+++ spec/pchamp_spec.rb
@@ -5,7 +5,23 @@ describe Pchamp do
     expect(Pchamp::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it 'reads files' do
     expect(false).to eq(true)
+  end
+
+  it 'isolates p statements' do
+    #code
+  end
+
+  it 'isolates puts statements' do
+    #code
+  end
+
+  it 'changes p statement a color' do
+    #code
+  end
+
+  it 'changes puts statement a color' do
+    #code
   end
 end
