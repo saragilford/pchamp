eb0171fd1634e2a4df6be590f3125ccb0512fc02
Sara Gilford <sara.gilford@gmail.com>
2015-07-21 15:41:38 -0700

    Update spec

--- spec/pchamp_spec.rb
+++ spec/pchamp_spec.rb
@@ -5,23 +5,4 @@ describe Pchamp do
     expect(Pchamp::VERSION).not_to be nil
   end
 
-  it 'reads files' do
-    expect(false).to eq(true)
-  end
-
-  it 'isolates p statements' do
-    #code
-  end
-
-  it 'isolates puts statements' do
-    #code
-  end
-
-  it 'changes p statement a color' do
-    #code
-  end
-
-  it 'changes puts statement a color' do
-    #code
-  end
 end
