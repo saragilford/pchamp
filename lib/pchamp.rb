1d006cb04da75a2e21284972e09646468b799d5e
Sara Gilford <sara.gilford@gmail.com>
2015-07-21 15:30:57 -0700

    Changed architecture for easier method calls

--- lib/pchamp.rb
+++ lib/pchamp.rb
@@ -1,27 +1,22 @@
 require "pchamp/version"
 require 'colorize'
 
-module Pchamp
-  extend self
+def pc(response)
+  if response.is_a? String
+    format_string(response)
+  else
+    format_data(response)
+  end
+end
 
-    def pc(response)
-      if response.is_a? String
-        format_string(response)
-      else
-        format_data(response)
-      end
-    end
+def format_string(string)
+  puts ('*' * 40).blue
+  puts "\"#{string}\"".green
+  puts ('*' * 40).blue
+end
 
-    def format_string(string)
-      puts ('*' * 40).blue
-      puts "\"#{string}\"".green
-      puts ('*' * 40).blue
-    end
-
-    def format_data(data)
-      puts ('*' * 40).blue
-      puts "#{data}".green
-      puts ('*' * 40).blue
-    end
-
+def format_data(data)
+  puts ('*' * 40).blue
+  puts "#{data}".green
+  puts ('*' * 40).blue
 end
